f025815732aaa5ed7afd2888f406f0139c28b6f3
zuzanka <hellingebleidd@gmail.com>
2020-09-23 12:03:51 +0200

    brokolica

--- src/paz1c/druhaJava.java
+++ src/paz1c/druhaJava.java
@@ -5,6 +5,7 @@ public class druhaJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("spagety carbonara");
+		System.out.println("vyprazana brokolica");
 	}
 
 }
